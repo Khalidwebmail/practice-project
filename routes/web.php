51ff121cf27c8627588456c50d336be3c2a1c45e
khalidwebmail <csewebmail@gmail.com>
2022-08-06 00:27:32 +0600

    how to use service provider

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd(app());
     return view('welcome');
 });
 
 Route::get('/test', function () {
     app()->make("first_service_provider");
     return "Test page";
+});
+
+Route::get('/test-one', function () {
+    return view('test');
 });
